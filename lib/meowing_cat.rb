100508007c262e84c2303d3ba6ab6ae4dac77ea9
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 03:35:05 +0300

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+   attr_accessor :name
+
+   #method
+   def meow
+    puts "meow!" 
+   end
+end 
+
+vert = Cat.new
+vert.meow
+
+
